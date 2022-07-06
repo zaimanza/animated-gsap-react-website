d6fa57d07a3cf754462660afb3886db004441a24
MANZA <36519974+zaimanza@users.noreply.github.com>
2022-07-06 15:20:55 +0800

    comment everything

--- src/App.js
+++ src/App.js
@@ -1,24 +1,82 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useEffect, useRef } from 'react';
+
+gsap.registerPlugin(ScrollTrigger);
 
 function App() {
+  // const squareRef = useRef();
+  // const div2Ref = useRef();
+
+  // function vhToPixels(vh) {
+  //   return Math.round(window.innerHeight / (100 / vh)) + 'px';
+  // }
+
+  // useEffect(() => {
+  //   gsap.to(div2Ref.current, {
+  //     // x: 700,
+  //     duration: 3,
+  //     scrollTrigger: {
+  //       trigger: squareRef.current,
+  //       start: "top 0%",
+  //       end: vhToPixels(400),
+  //       scrub: true,
+  //       toggleActions: "play none none none",
+  //       pin: true,
+  //       // pinSpacing: true,
+  //       markers: true,
+  //       // toggleClass: "background-color"
+
+  //     },
+  //   });
+  // }, [])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div></div>
+    // <div className="App" style={{
+    //   margin: 0
+    // }}
+    // >
+    //   <div className="div2"
+    //     ref={div2Ref}
+    //     style={{
+    //       height: "100vh",
+    //       "background-color": "salmon"
+    //     }}
+    //   >
+    //     <div className="square"
+    //       ref={squareRef}
+    //       style={{
+    //         width: "150px",
+    //         height: "100vh",
+    //         "background-color": "fuchsia"
+    //       }}
+    //     ></div>
+    //   </div>
+    //   <div className="div1" style={{
+    //     height: "100vh",
+    //     "background-color": "pink"
+    //   }}
+    //   ></div>
+    //   <div className="div1" style={{
+    //     height: "100vh",
+    //     "background-color": "green"
+    //   }}
+    //   ></div>
+    //   <div className="div1" style={{
+    //     height: "100vh",
+    //     "background-color": "blue"
+    //   }}
+    //   ></div>
+    //   <div className="div1" style={{
+    //     height: "100vh",
+    //     "background-color": "red"
+    //   }}
+    //   ></div>
+    // </div>
   );
 }
 
